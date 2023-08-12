0341119dd8d2978d82658f5e40fa78ab3cdaafda
sanjithsarkar <sanjithsarkar2@gmail.com>
2023-08-12 15:55:16 +0600

    Initial commit

---
+++ Admin/app/Http/Controllers/UserController.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+
+class UserController extends Controller
+{
+    public function Store(Request $request){
+        
+        $input = $request->all();
+        // $input['password'] = bcrypt($input['password']);
+        $input['password'] = Hash::make($request->password);
+        $user = User::create($input);
+
+        $success['token'] = $user->createToken('MyApp')->plainTextToken;
+        $success['name'] = $user->name;
+
+        $response = [
+            'success' => true,
+            'data' => $success,
+            'message' => 'User Register Successfully!!'
+        ];
+
+        return response()->json($response, 200);
+    }
+
+    public function Index(){
+        $users = User::all();
+
+        return response()->json(['users' => $users]);
+    }
+
+    public function Destroy(User $user){
+        $users = $user->delete();
+
+        return response()->json(['users' => $users]);
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# crud-with-react
+ 
