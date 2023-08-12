fc499ec1c615c5b24275c2272494adf004a13b36
sanjithsarkar <sanjithsarkar2@gmail.com>
2023-08-12 16:01:24 +0600

    Update Userlist.jsx

--- crud-app/src/Pages/Userlist.jsx
+++ crud-app/src/Pages/Userlist.jsx
@@ -56,7 +56,6 @@ const UserList = () => {
                         <tbody>
 
                             {users.map(user => (
-                                
                                 <tr key={user.id}>
                                     <td>{user.id}</td>
                                     <td>{user.name}</td>
